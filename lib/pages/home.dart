b56c90bba9d9d9f1ff230487a600d4489d9707bd
JayVnl <jorrit.v@outlook.com>
2020-01-09 21:58:45 +0100

    See description: - added few comments for readability - added errorhandling for google authentication login - added automatic reauthentication on reopening the app - added logout function

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-final GoogleSignIn googleSignIn =  GoogleSignIn();
+final GoogleSignIn googleSignIn = GoogleSignIn();
 
 class Home extends StatefulWidget {
   @override
@@ -13,28 +13,48 @@ class _HomeState extends State<Home> {
   bool isAuth = false;
 
   @override
-  void initState() { 
+  void initState() {
     super.initState();
-    googleSignIn.onCurrentUserChanged.listen((account){
-      if (account != null) {
-        print('User signed in!: $account');
-        setState(() {
-          isAuth = true;
-        });
-      } else {
-        setState(() {
-          isAuth = false;
-        });
-      }
+    // Detects when user signed in
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      handleSignIn(account);
+    }, onError: (err) {
+      print('Error signing in: $err');
     });
+    // Reauthenticate user when app is opened again
+    googleSignIn.signInSilently(suppressErrors: false).then((account) {
+      handleSignIn(account);
+    }).catchError((err) {
+      print('Error signing in: $err');
+    });
+  }
+
+  handleSignIn(GoogleSignInAccount account) {
+    if (account != null) {
+      print('User signed in!: $account');
+      setState(() {
+        isAuth = true;
+      });
+    } else {
+      setState(() {
+        isAuth = false;
+      });
+    }
   }
 
   login() {
     googleSignIn.signIn();
   }
 
+  logout() {
+    googleSignIn.signOut();
+  }
+
   Widget buildAuthScreen() {
-    return Text('Authenticated');
+    return RaisedButton(
+      child: Text('Logout'),
+      onPressed: logout,
+    );
   }
 
   Scaffold buildUnAuthScreen() {

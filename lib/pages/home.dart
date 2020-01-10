8fe5dde873d8aae947e53aacd51f0c2f061c038e
JayVnl <jorrit.v@outlook.com>
2020-01-10 23:01:09 +0100

    Changed inactive color of the navbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -100,6 +100,7 @@ class _HomeState extends State<Home> {
         currentIndex: pageIndex,
         onTap: onTap,
         activeColor: Theme.of(context).accentColor,
+        inactiveColor: Colors.white,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),

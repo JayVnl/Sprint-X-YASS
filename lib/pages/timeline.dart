cda6e3f96e05dbb6df12ce96335abf3e4a87cebb
JayVnl <jorrit.v@outlook.com>
2020-01-10 23:47:49 +0100

    Firestore connection established and tested

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,6 +1,10 @@
 import 'package:YASS/widgets/header.dart';
 import 'package:YASS/widgets/progress.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+// Assigning the collection named 'users' saved in the linked firestore database to the variable usersRef
+final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -8,6 +12,20 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  @override
+  void initState() {
+    getUsers();
+    super.initState();
+  }
+
+// Collect the current documents of the collection 'users' and then print out each document with their own fields
+  getUsers() async {
+    final QuerySnapshot snapshot = await usersRef.getDocuments();
+    snapshot.documents.forEach((DocumentSnapshot doc) {
+      print(doc.data);
+    });
+  }
+
   @override
   Widget build(context) {
     return Scaffold(

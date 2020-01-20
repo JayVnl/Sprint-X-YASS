7aef86edd4d7bebcb442b3fa9364b31584811aac
JayVnl <jorrit.v@outlook.com>
2020-01-20 23:02:41 +0100

    function that let's users like posts now works

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -64,6 +64,7 @@ class Post extends StatefulWidget {
 }
 
 class _PostState extends State<Post> {
+  final String currentUserId = currentUser?.id;
   final String postId;
   final String ownerId;
   final String username;
@@ -72,6 +73,7 @@ class _PostState extends State<Post> {
   final String mediaUrl;
   int likeCount;
   Map likes;
+  bool isLiked;
 
   _PostState({
     this.postId,
@@ -123,9 +125,37 @@ class _PostState extends State<Post> {
     );
   }
 
+  handleLikePost() {
+    bool _isLiked = likes[currentUserId] == true;
+
+    if (_isLiked) {
+      postsRef
+        .document(ownerId)
+        .collection('userPosts')
+        .document(postId)
+        .updateData({ 'likes.$currentUserId': false });
+      setState(() {
+        likeCount -= 1;
+        isLiked = false;
+        likes[currentUserId] = false;
+      });
+    } else if (!_isLiked) {
+      postsRef
+        .document(ownerId)
+        .collection('userPosts')
+        .document(postId)
+        .updateData({ 'likes.$currentUserId': true });
+      setState(() {
+        likeCount += 1;
+        isLiked = true;
+        likes[currentUserId] = true;
+      });
+    }
+   }
+
   buildPostImage() {
     return GestureDetector(
-      onDoubleTap: () => print("post liked"),
+      onDoubleTap: handleLikePost,
       child: Stack(
         alignment: Alignment.center,
         children: <Widget>[
@@ -145,9 +175,9 @@ class _PostState extends State<Post> {
               padding: EdgeInsets.only(top: 40.0, left: 20.0),
             ),
             GestureDetector(
-              onTap: () => print("post liked"),
+              onTap: handleLikePost,
               child: Icon(
-                Icons.favorite_border,
+                isLiked ? Icons.favorite : Icons.favorite_border,
                 size: 28.0,
                 color: Theme.of(context).accentColor,
               ),
@@ -208,6 +238,8 @@ class _PostState extends State<Post> {
 
   @override
   Widget build(BuildContext context) {
+    isLiked = (likes[currentUserId] == true);
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
